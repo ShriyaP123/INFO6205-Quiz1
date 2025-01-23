2a652bf02dc719aec777f9c0525eee21997fec6f
Shriya Pratapwar <pratapwar.s@northeastern.edu>
2025-01-23 13:06:24 -0500

    Bubble Sort implemented

--- src/main/java/BubbleSort.java
+++ src/main/java/BubbleSort.java
@@ -9,8 +9,14 @@ public class BubbleSort {
      *       This optimization ensures the algorithm stops early if the array is already sorted.
      */
     public void sort (Comparable[] a){
-//        write your code here.
-
+        int n = a.length;
+        for(int i=0; i<n-1; i++){
+            for(int j=0; j<n-i-1; j++){
+                if(a[j].compareTo(a[j+1])>0){
+                    swap(a, j, j+1);
+                }
+            }
+        }
     }
     /**
      * Swaps two elements in the given array.
